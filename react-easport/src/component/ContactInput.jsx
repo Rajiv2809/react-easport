882537adca54f8588b650035dbf94d83110eb01a
Rajiv2809 <121161360+Rajiv2809@users.noreply.github.com>
2023-11-11 21:27:52 +0700

    main

---
+++ react-easport/src/component/ContactInput.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+class ContactInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      tag: "",
+    };
+
+    this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+    this.onTagChangeEventHandler = this.onTagChangeEventHandler.bind(this);
+    this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+  }
+
+  onNameChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        name: event.target.value,
+      };
+    });
+  }
+
+  onTagChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        tag: event.target.value,
+      };
+    });
+  }
+
+  onSubmitEventHandler(event) {
+    event.preventDefault();
+    this.props.addContact(this.state);
+
+    
+    this.setState({
+      name: "",
+      tag: "",
+    });
+  }
+
+  render() {
+    return (
+      <form className="contact-input" onSubmit={this.onSubmitEventHandler}>
+        <input
+          type="text"
+          placeholder="Nama"
+          value={this.state.name}
+          onChange={this.onNameChangeEventHandler}
+        />
+        <input
+          type="text"
+          placeholder="Tag"
+          value={this.state.tag}
+          onChange={this.onTagChangeEventHandler}
+        />
+        <div className="submit">
+
+          <button type="submit">Tambah</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default ContactInput;
---
+++ react-easport/src/utils/data.js
@@ -0,0 +1,26 @@
+
+const getData = () => {
+    return [
+      {
+        id: 1,
+        name: 'Kevin ',
+        tag: 'madman',
+        imageUrl: 'https://i.pinimg.com/736x/73/65/35/736535cc191c107a9e3a322de1f76950.jpg',
+      },
+      {
+        id: 2,
+        name: 'narosa',
+        tag: 'rose_1',
+        imageUrl: 'https://i.pinimg.com/736x/bd/e5/1e/bde51e480ff5308073d30875a3010a91.jpg',
+      },
+      {
+        id: 3,
+        name: 'rasya',
+        tag: 'raysa',
+        imageUrl: 'https://i.pinimg.com/564x/ed/79/b8/ed79b88276139af8a7bdd9b3c82d8a01.jpg',
+      },
+    ];
+  }
+  
+  export { getData };
+  
